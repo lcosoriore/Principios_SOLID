57a206f16350e6d63ed29ff4b7108935d3b9b1bd
lcosoriore <69127397+lcosoriore@users.noreply.github.com>
2023-03-03 11:49:17 -0500

    Se agrega clase ExportHelperGeneric para poder recibir cualquier tipo deo bjeto y exportarlo a un archivo csv

---
+++ 1-SingleResponsability/ExportHelperGeneric.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsability
+{
+    public class ExportHelperGeneric<T>
+    {
+        public void ExportToCSV(IEnumerable<T> items)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            string header = "";
+            string[] dataRows = new string[items.Count()];
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                header += $"{prop.Name};";
+                for (int i = 0; i < items.Count(); i++)
+                {
+                    var propValue = prop.GetValue(items.ToArray()[i]);
+                    var propType = propValue.GetType();
+                    if (propType.Name != nameof(String)
+                        && propType.GetInterface(nameof(IEnumerable)) != null)
+                    {
+                        dataRows[i] += $"{String.Join("|", (propValue as IEnumerable).Cast<object>().Select(x => x.ToString()))};";
+
+                    }
+                    else
+                    {
+                        dataRows[i] += $"{propValue};";
+                    }
+                }
+            }
+            sb.AppendLine(header.Trim(';'));
+            foreach (var dataRow in dataRows)
+            {
+                sb.AppendLine(dataRow.Trim(';'));
+            }
+            System.IO.File.WriteAllText(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"Export_{typeof(T).ToString()}.csv"), sb.ToString(), Encoding.Unicode);
+        }
+    }
+}
--- 1-SingleResponsability/Program.cs
+++ 1-SingleResponsability/Program.cs
@@ -2,5 +2,9 @@
 
 StudentRepository studentRepository = new();
 ExportHelper exportHelper = new();
+ExportHelperGeneric<Student> exportHelperGeneric = new();
+// Se exporta informacion a archico csv enviando un objeto especifico
 exportHelper.ExportStudent(studentRepository.GetAll());
+//Exporta la informacion a archivo csv enviando cual quier tipo de objeto
+exportHelperGeneric.ExportToCSV(studentRepository.GetAll());
 Console.WriteLine("Proceso Completado");
